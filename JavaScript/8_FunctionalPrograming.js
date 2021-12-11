def08074e4a0ae40a17e8148c70be570826bbcf6
crislav10 <cristianlav10@gmail.com>
2021-12-11 13:08:32 -0500

    Intermediate Algorithm Scripting 2

--- JavaScript/8_FunctionalPrograming.js
+++ JavaScript/8_FunctionalPrograming.js
@@ -809,3 +809,73 @@ function add(x) {
 }
 
 add(10)(20)(30);
+
+
+
+/*Wherefore art thou
+Make a function that looks through an array of objects (first argument) and returns an array of all objects that have matching name and value pairs (second argument). Each name and value pair of the source object has to be present in the object from the collection if it is to be included in the returned array.
+
+For example, if the first argument is [{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], and the second argument is { last: "Capulet" }, then you must return the third object from the array (the first argument), because it contains the name and its value, that was passed on as the second argument.*/
+
+function whatIsInAName(collection, source) {
+  const arr = [];
+  let keys=Object.keys(source);
+  let filter=collection.filter(obj=> {
+    for (var i = 0; i < keys.length; i++) {
+      if (
+        !obj.hasOwnProperty(keys[i]) ||
+        obj[keys[i]] !== source[keys[i]]
+      ) {
+        return false;
+      }
+    }
+    return true;
+  });
+  return filter;
+}
+
+
+
+function whatIsInAName(collection, source) {
+  // "What's in a name? that which we call a rose
+  // By any other name would smell as sweet.”
+  // -- by William Shakespeare, Romeo and Juliet
+  var srcKeys = Object.keys(source);
+
+  return collection.filter(function(obj) {
+    return srcKeys.every(function(key) {
+      return obj.hasOwnProperty(key) && obj[key] === source[key];
+    });
+  });
+}
+
+
+function whatIsInAName(collection, source) {
+  // "What's in a name? that which we call a rose
+  // By any other name would smell as sweet.”
+  // -- by William Shakespeare, Romeo and Juliet
+  var srcKeys = Object.keys(source);
+
+  // filter the collection
+  return collection.filter(function(obj) {
+    return srcKeys
+      .map(function(key) {
+        return obj.hasOwnProperty(key) && obj[key] === source[key];
+      })
+      .reduce(function(a, b) {
+        return a && b;
+      });
+  });
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+/*Spinal Tap Case
+Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.*/
+
+function spinalCase(str) {
+  let arr=str.split(/\s|_|(?=[A-Z])/)
+  .join('-').toLowerCase();
+  return arr;
+}
+
+console.log(spinalCase("The_Andy_Griffith_Show"));
