ed0f28a4a4932d79d480c16e02ced9d2a03bf1e9
crislav10 <cristianlav10@gmail.com>
2021-12-14 12:16:27 -0500

    Intermediate Algorithm Scripting 3

--- JavaScript/9_IntermediateAS.js
+++ JavaScript/9_IntermediateAS.js
@@ -204,3 +204,237 @@ Translate the provided string to Pig Latin. Input strings are guaranteed to be E
     return pigLatin;
   }
   translatePigLatin("consonant"); 
+/*Search and Replace
+Perform a search and replace on the sentence using the arguments provided and return the new sentence.
+
+First argument is the sentence to perform the search and replace on.
+
+Second argument is the word that you will be replacing (before).
+
+Third argument is what you will be replacing the second argument with (after).
+
+Note: Preserve the case of the first character in the original word when you are replacing it. For example if you mean to replace the word Book with the word dog, it should be replaced as Dog*/
+
+function myReplace(str, before, after) {
+  if(/^[A-Z]/.test(before)) {
+    var arr=after.split('')
+    arr[0]=arr[0].toUpperCase();
+    after=arr.join('');
+    }else{after=after.toLowerCase()};
+  
+  let regex =new RegExp(before);
+  let result=str.replace(regex,after);
+  return result;
+}
+
+console.log(myReplace("I think we should look up there", "up", "Down"));
+
+function myReplace(str, before, after) {
+  // Find index where before is on string
+  var index = str.indexOf(before);
+  // Check to see if the first letter is uppercase or not
+  if (str[index] === str[index].toUpperCase()) {
+    // Change the after word to be capitalized before we use it.
+    after = after.charAt(0).toUpperCase() + after.slice(1);
+  } else {
+    // Change the after word to be uncapitalized before we use it.
+    after = after.charAt(0).toLowerCase() + after.slice(1);
+  }
+  // Now replace the original str with the edited one.
+  str = str.replace(before, after);
+
+  return str;
+}
+
+//https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String/substring
+function myReplace(str, before, after) {
+  // Check if first character of argument "before" is a capital or lowercase letter and change the first character of argument "after" to match the case
+  if (/^[A-Z]/.test(before)) {
+    after = after[0].toUpperCase() + after.substring(1)
+  } else {
+    after = after[0].toLowerCase() + after.substring(1)
+  }
+
+  // return string with argument "before" replaced by argument "after" (with correct case)
+  return str.replace(before, after);
+}
+
+function myReplace(str, before, after) {
+  // create a function that will change the casing of any number of letter in parameter "target"
+  // matching parameter "source"
+  function applyCasing(source, target) {
+    // split the source and target strings to array of letters
+    var targetArr = target.split("");
+    var sourceArr = source.split("");
+    // iterate through all the items of sourceArr and targetArr arrays till loop hits the end of shortest array
+    for (var i = 0; i < Math.min(targetArr.length, sourceArr.length); i++) {
+      // find out the casing of every letter from sourceArr using regular expression
+      // if sourceArr[i] is upper case then convert targetArr[i] to upper case
+      if (/[A-Z]/.test(sourceArr[i])) {
+        targetArr[i] = targetArr[i].toUpperCase();
+      }
+      // if sourceArr[i] is not upper case then convert targetArr[i] to lower case
+      else targetArr[i] = targetArr[i].toLowerCase();
+    }
+    // join modified targetArr to string and return
+    return targetArr.join("");
+  }
+
+  // replace "before" with "after" with "before"-casing
+  return str.replace(before, applyCasing(before, after));
+}
+
+
+// Add new method to the String object, not overriding it if one exists already
+String.prototype.capitalize =
+  String.prototype.capitalize ||
+  function() {
+    return this[0].toUpperCase() + this.slice(1);
+  };
+
+const Util = (function() {
+  // Create utility module to hold helper functions
+  function textCase(str, tCase) {
+    // Depending if the tCase argument is passed we either set the case of the
+    // given string or we get it.
+    // Those functions can be expanded for other text cases.
+
+    if (tCase) {
+      return setCase(str, tCase);
+    } else {
+      return getCase(str);
+    }
+
+    function setCase(str, tCase) {
+      switch (tCase) {
+        case "uppercase":
+          return str.toUpperCase();
+        case "lowercase":
+          return str.toLowerCase();
+        case "capitalized":
+          return str.capitalize();
+        default:
+          return str;
+      }
+    }
+
+    function getCase(str) {
+      if (str === str.toUpperCase()) {
+        return "uppercase";
+      }
+      if (str === str.toLowerCase()) {
+        return "lowercase";
+      }
+      if (str === str.capitalize()) {
+        return "capitalized";
+      }
+      return "normal";
+    }
+  }
+
+  return {
+    textCase
+  };
+})();
+
+function myReplace(str, before, after) {
+  const { textCase } = Util;
+  const regex = new RegExp(before, "gi");
+  const replacingStr = textCase(after, textCase(before));
+
+  return str.replace(regex, replacingStr);
+}
+
+function myReplace(str, before, after) {
+  const myArr = str.split(" ");
+  const [wordToReplace] = myArr.filter(item => item === before);
+  return wordToReplace[0].toUpperCase() !== wordToReplace[0]
+    ? myArr.map(item => (item === before ? after : item)).join(" ")
+    : myArr
+        .map(item =>
+          item === before ? after[0].toUpperCase() + after.slice(1) : item
+        )
+        .join(" ");
+}
+
+
+/*DNA Pairing
+The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array.
+
+Base pairs are a pair of AT and CG. Match the missing element to the provided character.
+
+Return the provided character as the first element in each array.
+
+For example, for the input GCG, return [["G", "C"], ["C","G"], ["G", "C"]]
+
+The character and its pair are paired up in an array, and all the arrays are grouped into one encapsulating array.*/
+
+function pairElement(str) {
+  let arr=[];
+  let strSplit=str.split('');
+  for(let i=0;i<strSplit.length;i++){
+    switch(strSplit[i]){
+        case "A": arr.push(['A','T']);
+                  break;
+        case "T": arr.push(['T','A']);
+                  break;
+        case "G": arr.push(['G','C']);
+                  break;
+        case "C": arr.push(['C','G']);
+                  break;
+      };
+  }
+  return arr;
+}
+
+function pairElement(str) {
+  // Return each strand as an array of two elements, the original and the pair.
+  var paired = [];
+
+  // Function to check with strand to pair.
+  var search = function(char) {
+    switch (char) {
+      case "A":
+        paired.push(["A", "T"]);
+        break;
+      case "T":
+        paired.push(["T", "A"]);
+        break;
+      case "C":
+        paired.push(["C", "G"]);
+        break;
+      case "G":
+        paired.push(["G", "C"]);
+        break;
+    }
+  };
+
+  // Loops through the input and pair.
+  for (var i = 0; i < str.length; i++) {
+    search(str[i]);
+  }
+
+  return paired;
+}
+
+function pairElement(str) {
+  //create object for pair lookup
+  var pairs = {
+    A: "T",
+    T: "A",
+    C: "G",
+    G: "C"
+  };
+  //split string into array of characters
+  var arr = str.split("");
+  //map character to array of character and matching pair
+  return arr.map(x => [x, pairs[x]]);
+}
+
+console.log(pairElement("AAA"));
+
+
+/*Missing letters
+Find the missing letter in the passed letter range and return it.
+
+If all letters are present in the range, return undefined.*/
