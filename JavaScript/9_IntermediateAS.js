5508f7e0a826e33cecb169a13ca1a400955af22d
crislav10 <cristianlav10@gmail.com>
2021-12-15 15:46:20 -0500

    Intermediate Algorithm Scripting 3

--- JavaScript/9_IntermediateAS.js
+++ JavaScript/9_IntermediateAS.js
@@ -438,3 +438,234 @@ console.log(pairElement("AAA"));
 Find the missing letter in the passed letter range and return it.
 
 If all letters are present in the range, return undefined.*/
+
+function fearNotLetter(str) {
+  for (var i = 0; i < str.length; i++) {
+    /* code of current character */
+    var code = str.charCodeAt(i);
+
+    /* if code of current character is not equal to first character + no of iteration
+        hence character has been escaped */
+    if (code !== str.charCodeAt(0) + i) {
+      /* if current character has escaped one character find previous char and return */
+      return String.fromCharCode(code - 1);
+    }
+  }
+  return undefined;
+}
+
+function fearNotLetter(str) {
+  for (let i=0;i<str.length;i++){
+    let asciiCode=str.charCodeAt(i);
+    if(asciiCode!=str.charCodeAt(0)+i){
+      return String.fromCharCode(asciiCode-1);
+      }
+  }
+  return undefined;
+}
+
+function fearNotLetter(str) {
+  let currCharCode = str.charCodeAt(0);
+  let missing = undefined;
+
+  str
+    .split("")
+    .forEach(letter => {
+      if (letter.charCodeAt(0) === currCharCode) {
+        currCharCode++;
+      } else {
+        missing = String.fromCharCode(currCharCode);
+      }
+    });
+
+  return missing;
+}
+// test here
+fearNotLetter("abce");
+
+/*Sorted Union
+Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+
+In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+Check the assertion tests for examples.*/
+
+function uniteUnique(...arr) {
+  let union=[];
+  for(let i=0;i<arr.length;i++){
+    let internarr=arr[i]
+    for(let value in internarr){
+      if(union.includes(internarr[value])) continue;
+      union.push(internarr[value]);
+    }
+  }
+  return union;
+}
+
+function uniteUnique(arr1, arr2, arr3) {
+  // Creates an empty array to store our final result.
+  var finalArray = [];
+
+  // Loop through the arguments object to truly make the program work with two or more arrays
+  // instead of 3.
+  for (var i = 0; i < arguments.length; i++) {
+    var arrayArguments = arguments[i];
+
+    // Loops through the array at hand
+    for (var j = 0; j < arrayArguments.length; j++) {
+      var indexValue = arrayArguments[j];
+
+      // Checks if the value is already on the final array.
+      if (finalArray.indexOf(indexValue) < 0) {
+        finalArray.push(indexValue);
+      }
+    }
+  }
+
+  return finalArray;
+}
+
+function uniteUnique(arr) {
+  var args = [...arguments];
+  var result = [];
+  for (var i = 0; i < args.length; i++) {
+    for (var j = 0; j < args[i].length; j++) {
+      if (!result.includes(args[i][j])) {
+        result.push(args[i][j]);
+      }
+    }
+  }
+  return result;
+}
+
+function uniteUnique(arr1, arr2, arr3) {
+  var newArr;
+  //Convert the arguments object into an array
+  var args = Array.prototype.slice.call(arguments);
+  //Use reduce function to flatten the array
+  newArr = args.reduce(function(arrA, arrB) {
+    //Apply filter to remove the duplicate elements in the array
+    return arrA.concat(
+      arrB.filter(function(i) {
+        return arrA.indexOf(i) === -1;
+      })
+    );
+  });
+
+  return newArr;
+}
+
+function uniteUnique() {
+  var concatArr = [];
+  var i = 0;
+  while (arguments[i]) {
+    concatArr = concatArr.concat(arguments[i]);
+    i++;
+  }
+  uniqueArray = concatArr.filter(function(item, pos) {
+    return concatArr.indexOf(item) == pos;
+  });
+  return uniqueArray;
+}
+
+//jshint esversion:6
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+function uniteUnique(...arrays) {
+  //make an array out of the given arrays and flatten it (using the spread operator)
+  const flatArray = [].concat(...arrays);
+
+  // create a Set which clears any duplicates since it's a regular set and not a multiset
+  return [...new Set(flatArray)];
+}
+
+function uniteUnique(...arr) {
+  return [...new Set(arr.flat())]
+}
+
+// Or as an arrow function
+const uniteUnique = (...arr) => [...new Set(arr.flat())]
+
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]));
+
+
+/*Convert HTML Entities
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.*/
+
+
+function convertHTML(str) {
+  const htmlEntities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;"
+  };
+  let regex=/([&<>\"'])/g;
+  return str.replace(regex,match=>htmlEntities[match]);
+}
+
+function convertHTML(str) {
+  // Use Object Lookup to declare as many HTML entities as needed.
+  const htmlEntities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;"
+  };
+  //Use map function to return a filtered str with all entities changed automatically.
+  return str
+    .split("")
+    .map(entity => htmlEntities[entity] || entity)
+    .join("");
+}
+
+function convertHTML(str) {
+  // Use Object Lookup to declare as many HTML entities as needed.
+  const htmlEntities = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': "&quot;",
+    "'": "&apos;"
+  };
+  // Using a regex, replace characters with it's corresponding html entity
+  return str.replace(/([&<>\"'])/g, match => htmlEntities[match]);
+}
+
+function convertHTML(str) {
+  // Split by character to avoid problems.
+
+  var temp = str.split("");
+
+  // Since we are only checking for a few HTML elements, use a switch
+
+  for (var i = 0; i < temp.length; i++) {
+    switch (temp[i]) {
+      case "<":
+        temp[i] = "&lt;";
+        break;
+      case "&":
+        temp[i] = "&amp;";
+        break;
+      case ">":
+        temp[i] = "&gt;";
+        break;
+      case '"':
+        temp[i] = "&quot;";
+        break;
+      case "'":
+        temp[i] = "&apos;";
+        break;
+    }
+  }
+
+  temp = temp.join("");
+  return temp;
+}
+
+
+
+console.log(convertHTML("Dolce & Gabbana"));
