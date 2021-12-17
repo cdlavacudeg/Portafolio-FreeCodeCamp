21278190417aa9eef7afb37dcbf9e23e084a2baf
crislav10 <cristianlav10@gmail.com>
2021-12-17 15:59:29 -0500

    JavaScript Palindrome Checker Project

---
+++ JavaScript/P1_PallindromeCheker.js
@@ -0,0 +1,22 @@
+/*Palindrome Checker
+Return true if the given string is a palindrome. Otherwise, return false.
+
+A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+
+Note: You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
+
+We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR among others.
+
+We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2.*/
+
+function palindrome(str) {
+    let arr=str.toLowerCase().match(/[A-Za-z0-9]/g);
+    
+    let invarr=[];
+    for(let i=arr.length-1;i>=0;i--){
+      invarr.push(arr[i]);
+    }
+    return invarr.join('')==arr.join('');
+  }
+  
+  console.log(palindrome("_eye"));
