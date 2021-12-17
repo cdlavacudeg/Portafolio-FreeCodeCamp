d66f574826e95eaeedee51be256bb9091fd32d41
crislav10 <cristianlav10@gmail.com>
2021-12-17 15:41:46 -0500

    Intermediate Algorithm Scripting

--- JavaScript/9_IntermediateAS.js
+++ JavaScript/9_IntermediateAS.js
@@ -1227,3 +1227,180 @@ console.log(truthCheck([
   }
   
   console.log(addTogether(2)(2));
+
+  /*Make a Person
+Fill in the object constructor with the following methods below:
+
+getFirstName()
+getLastName()
+getFullName()
+setFirstName(first)
+setLastName(last)
+setFullName(firstAndLast)
+Run the tests to see the expected output for each method. The methods that take an argument must accept only one argument and it has to be a string. These methods must be the only available means of interacting with the object.*/
+
+const Person = function(firstAndLast) {
+  let FullName =firstAndLast;
+  let FirstName=firstAndLast.split(' ')[0];
+  let LastName=firstAndLast.split(' ')[1];
+
+  this.getFullName = function() {
+    return FullName;
+  };
+
+  this.getFirstName = function() {
+    return FirstName;
+  };
+
+  this.getLastName = function() {
+    return LastName;
+  };
+
+  this.setFirstName = function(a){
+    FirstName=a;
+    FullName=a+' '+LastName;
+  };
+  this.setLastName = function(a){
+    LastName=a;
+    FullName=FirstName+' '+a;
+  };
+  this.setFullName = function(a){
+    FullName=a;
+    FirstName=a.split(' ')[0];
+    LastName=a.split(' ')[1];
+  };
+
+  return firstAndLast;
+};
+
+var Person = function(firstAndLast) {
+  var fullName = firstAndLast;
+
+  this.getFirstName = function() {
+    return fullName.split(" ")[0];
+  };
+
+  this.getLastName = function() {
+    return fullName.split(" ")[1];
+  };
+
+  this.getFullName = function() {
+    return fullName;
+  };
+
+  this.setFirstName = function(name) {
+    fullName = name + " " + fullName.split(" ")[1];
+  };
+
+  this.setLastName = function(name) {
+    fullName = fullName.split(" ")[0] + " " + name;
+  };
+}
+const bob = new Person('Bob Ross');
+bob.setFirstName('Pablo');
+console.log(bob.getFullName());
+
+
+/*Map the Debris
+Return a new array that transforms the elements' average altitude into their orbital periods (in seconds).
+
+The array will contain objects in the format {name: 'name', avgAlt: avgAlt}.
+
+You can read about orbital periods on Wikipedia.
+
+The values should be rounded to the nearest whole number. The body being orbited is Earth.
+
+The radius of the earth is 6367.4447 kilometers, and the GM value of earth is 398600.4418 km3s-2.*/
+
+function orbitalPeriod(arr) {
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+  const two_pi=2*Math.PI;
+  const newarr=[];
+
+  const getOrbitalP=(obj)=>{
+    const numerator=Math.pow(earthRadius+obj.avgAlt,3);
+    const sqrt=Math.sqrt(numerator/GM);
+    const orbP=Math.round(two_pi*sqrt);
+    return {name:obj.name,orbitalPeriod:orbP};
+  }
+
+  for(let i=0;i<arr.length;i++){
+    newarr.push(getOrbitalP(arr[i]));
+  }
+
+
+  return newarr;
+}
+
+function orbitalPeriod(arr) {
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+  const a = 2 * Math.PI;
+  const newArr = [];
+
+  const getOrbPeriod = function(obj) {
+    const c = Math.pow(earthRadius + obj.avgAlt, 3);
+    const b = Math.sqrt(c / GM);
+    const orbPeriod = Math.round(a * b);
+    // create new object
+    return {name: obj.name, orbitalPeriod: orbPeriod};
+  };
+
+  for (let elem in arr) {
+    newArr.push(getOrbPeriod(arr[elem]));
+  }
+
+  return newArr;
+}
+
+function orbitalPeriod(arr) {
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+  const newArr = [];
+
+  //Looping through each key in arr object
+  for (let elem in arr) {
+    //Rounding off the orbital period value
+    const orbitalPer = Math.round(
+      2 * Math.PI * Math.sqrt(Math.pow(arr[elem].avgAlt + earthRadius, 3) / GM)
+    );
+    //Adding new object with orbitalPeriod property
+    newArr.push({name: arr[elem].name, orbitalPeriod: orbitalPer});
+  }
+
+  return newArr;
+}
+
+function orbitalPeriod(arr) {
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+  // Create new array to prevent modification of the original
+  const newArr = JSON.parse(JSON.stringify(arr));
+  // Loop through each item in the array arr
+  newArr.forEach(function(item) {
+    // Calculate the Orbital period value
+    const tmp = Math.round(
+      2 * Math.PI * Math.sqrt(Math.pow(earthRadius + item.avgAlt, 3) / GM)
+    );
+    //Delete the avgAlt property
+    delete item.avgAlt;
+    //Add orbitalPeriod property
+    item.orbitalPeriod = tmp;
+  });
+
+  return newArr;
+}
+
+
+function orbitalPeriod(arr) {
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+  return arr.map(({ name, avgAlt }) => {
+    const earth = earthRadius + avgAlt;
+    const orbitalPeriod = Math.round(2 * Math.PI * Math.sqrt(Math.pow(earth, 3)/GM));
+    return { name, orbitalPeriod };
+  });
+}
+
+console.log(orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]));
