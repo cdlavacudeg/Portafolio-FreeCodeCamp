4de7609ee1248fe783224704adee166f6c9f1e1b
crislav10 <cristianlav10@gmail.com>
2022-02-24 10:20:25 -0500

    Destination Tabs

--- DesignSystemCss/tabs.js
+++ DesignSystemCss/tabs.js
@@ -14,16 +14,17 @@ function changeTabFocus(e){
 
     if (e.keyCode === keydownLeft || e.keyCode===keydownRight) { 
         tabs[tabFocus].setAttribute("tabindex",-1);
+        if (e.keyCode==keydownRight){
+            tabFocus++;
+            if(tabFocus>=tabs.length) tabFocus=0;   
+        }
+        else if (e.keyCode==keydownLeft){
+            tabFocus--;
+            if(tabFocus<0) tabFocus=tabs.length-1;
+        }
     }
     
-    if (e.keyCode==keydownRight){
-        tabFocus++;
-        if(tabFocus>=tabs.length) tabFocus=0;   
-    }
-    if (e.keyCode==keydownLeft){
-        tabFocus--;
-        if(tabFocus<0) tabFocus=tabs.length-1;
-    }
+    
     tabs[tabFocus].setAttribute("tabindex",0);
     tabs[tabFocus].focus();
 }
@@ -41,18 +42,20 @@ function changeTapPanel(e){
         .setAttribute("aria-selected",false);
     
     targetTab.setAttribute("aria-selected",true);
+    
+    hideContent(mainContainer,'[role="tabpanel"]');
+    showContent(mainContainer,[`#${targetPanel}`]);
 
-    mainContainer
-        .querySelectorAll('[role="tabpanel"]')
-        .forEach((panel)=>panel.setAttribute('hidden',true));
+    hideContent(mainContainer,'picture');
+    showContent(mainContainer,[`#${targetImage}`]);
+}
 
-    mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
+function hideContent(parent,content){
+    parent
+        .querySelectorAll(content)
+        .forEach((item)=>item.setAttribute('hidden',true));
+}
 
-    mainContainer
-        .querySelectorAll('picture')
-        .forEach((picture)=>picture.setAttribute('hidden',true));
-
-    mainContainer.querySelector([`#${targetImage}`]).removeAttribute('hidden');
-
-    console.log(mainContainer);
+function showContent(parent,content){
+    parent.querySelector(content).removeAttribute('hidden');
 }
